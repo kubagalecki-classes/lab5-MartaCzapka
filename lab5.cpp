bc328bad44a95e326ab385b9818764f523d220fb
MartaCzapka <01123769@pw.edu.pl>
2020-12-03 12:50:10 +0000

    zad 15

--- lab5.cpp
+++ lab5.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 int main()
 {
-    list< int > lista;
+    list< int >           lista;
+    list< int >::iterator it = lista.begin();
     lista.push_back(1);
     for (int i = 0; i < 6; i++) {
         lista.push_front(i);
@@ -13,4 +14,15 @@ int main()
     cout << lista.front() << " " << &lista.front() << " ";
     cout << lista.back() << " " << &lista.back() << "\n";
     // cout << lista[2];
+    for (int i = 0; i < 6; i++) {
+        cout << *it << " ";
+        *it++;
+    }
+    cout << "\n";
+    list< int >::const_reverse_iterator itt = lista.rbegin();
+    // *itt--;
+    for (int i = 0; i < 6; i++) {
+        cout << *itt << " ";
+        *itt++;
+    }
 }
