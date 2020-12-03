3fa75b1b43a7ae522c3591ceb09b56b9dc6fe293
MartaCzapka <01123769@pw.edu.pl>
2020-12-03 12:35:00 +0000

    zad 11

--- lab5.cpp
+++ lab5.cpp
@@ -1,26 +1,16 @@
 #include "Human.hpp"
 #include <iostream>
+#include <list>
 #include <string>
-#include <vector>
 using namespace std;
 int main()
 {
-    vector< Human > wekt;
-    string          name;
-    for (int i = 0; i < 3; i++) {
-        cout << "Podaj imie";
-        cin >> name;
-        if (name == "pop") {
-            wekt.pop_back();
-        }
-        else {
-            Human temp{name, 8, true, true};
-            wekt.emplace_back(temp);
-        }
+    list< int > lista;
+    lista.push_back(1);
+    for (int i = 0; i < 6; i++) {
+        lista.push_front(i);
     }
-    cout << wekt.size() << " " << wekt.capacity() << "\n";
-    for (int i = 0; i < 2; i++) {
-
-        cout << wekt[i].getName() << " ";
-    }
+    cout << lista.front() << " " << &lista.front() << " ";
+    cout << lista.back() << " " << &lista.back() << "\n";
+    // cout << lista[2];
 }
