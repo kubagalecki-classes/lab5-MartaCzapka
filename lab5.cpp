2629ba7345d4097bb10fc899be367a1c69da465f
MartaCzapka <01123769@pw.edu.pl>
2020-12-03 12:01:11 +0000

    zad 2

--- lab5.cpp
+++ lab5.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
-
+#include <vector>
+#include "Human.hpp"
+using namespace std;
 int main()
 {
-    puts("Hello, World!");
+    vector< int > wekt;
+    cout << wekt.size() << " " << wekt.capacity() << "\n";
+    for (int i = 0; i < 100; i++) {
+        wekt.emplace_back(i + 8);
+        // cout << wekt.size() << " " << wekt.capacity() << "\n";
+        cout << &wekt[0] << "\n ";
+    }
+    cout << wekt.size() << " " << wekt.capacity() << "\n";
+    for (int i = 0; i < 100; i++) {
+
+        // cout << wekt[i] << " ";
+    }
 }
